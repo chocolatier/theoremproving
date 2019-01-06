85d6845f4f3284e4e4daa34d4befb9589b6b0ef4
chocolatier <chocolatier@users.noreply.github.com>
2019-01-07 02:46:07 +1100

    gauss attempt #2 start (note : incorrect, garbage)

---
+++ src/gauss_lemma.lean
@@ -0,0 +1,38 @@
+import data.polynomial
+import ring_theory.unique_factorization_domain
+
+open polynomial
+open classical
+
+universes u 
+
+variables {α : Type u} {a: α} 
+
+variables [integral_domain α] {p q r s : polynomial α}
+variables [decidable_eq α]
+variables [unique_factorization_domain α]
+
+-- 
+theorem has_R_root_imp_has_frac_R_root (p : polynomial α) : (p = p) := sorry
+
+def has_non_unit_divisor (a : α) : Prop := ∃(b : α), (¬is_unit b) ∧ (b ∣ a)
+
+lemma non_unit_div_ab_imp_non_unit_div_a_or_non_unit_div_b (a b : α) : ∃(c : α), 
+    (¬is_unit c) ∧ (c ∣ (a * b)) → (has_non_unit_divisor a) ∨ (has_non_unit_divisor b) := sorry
+
+def primitive (p : polynomial α) : Prop :=
+¬(∃(a : α), ∃(r : polynomial α), (¬is_unit a) ∧ p = (C a) * r)
+
+-- lemma not_prim_imp_factors (p : polynomial α) [¬primitive p] : ∃(a : α), ()
+
+lemma prod_of_prim_is_prim (p q : polynomial α) [primitive p] [primitive q] : primitive (p * q) :=
+by_contradiction
+    (assume ch : ¬(primitive (p * q)),
+        have ch2 : ¬¬(∃(a : α), ∃(r : polynomial α), (¬is_unit a) ∧ (p * q = C a * r)), from ch,
+        have ch2_5 : (∃(a : α), ∃(r : polynomial α), (¬is_unit a) ∧ (p * q = C a * r)), from sorry, -- double not elim on ch2
+        have ch3 : leading_coeff (p * q) = (leading_coeff p) * (leading_coeff q), by simp,
+        have ch4 :  (has_non_unit_divisor (leading_coeff p)) ∨ (has_non_unit_divisor (leading_coeff p)), by sorry,
+        
+        show false, from sorry)
+
+
