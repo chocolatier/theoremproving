a7a2153ff22d55d9ad3d89a82b69cf5536dcfff9
chocolatier <chocolatier@users.noreply.github.com>
2019-01-16 13:21:30 +1100

    fuck

--- src/gauss_lemma.lean
+++ src/gauss_lemma.lean
@@ -54,7 +54,3 @@ def mod_by_non_unit_const : Π (p : polynomial α) {q : polynomial α},
 def const_divisor : Π (p : polynomial α) (q : polynomial α), is_const q → Prop 
 | p q := λ hq,
     mod_by_const p hq = 0
-
-def primitive (p : polynomial α) {q : polynomial α} : non_unit_const q → Prop := λ hq, ¬(const_divisor p q (and.left hq))
-
-lemma prod_prim_is_prim (p : polynomial α) (q : polynomial α) : (primitive p _ _) ∧ (primitive q _ _) → primitive (p * q)
