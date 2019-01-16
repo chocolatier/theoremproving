8d4e107724a326afcd15da91d247f5a886e219cb
chocolatier <chocolatier@noreply.xyz>
2019-01-17 02:12:14 +1100

    prod prim skeleton

--- src/gauss_lemma.lean
+++ src/gauss_lemma.lean
@@ -60,7 +60,7 @@ def mod_by_non_unit_const : Π (p : polynomial α) {q : polynomial α},
     have hc : is_const q := and.left hq,
     mod_by_const p hc
 
-def const_divisor : Π (p : polynomial α) (q : polynomial α), is_const q → Prop 
+def const_divisor : Π (p : polynomial α) {q : polynomial α}, is_const q → Prop 
 | p q := λ hq,
     mod_by_const p hq = 0
 
@@ -69,13 +69,21 @@ def primitive (p : polynomial α) : Prop := ∀(q : polynomial α) (hq: non_unit
 
 instance primitive.decidable : decidable (primitive p) := sorry
 
+lemma h_div_lemma {p : polynomial α} (hp : ¬primitive p) : ∃(m : polynomial α) (hm : non_unit_const m), mod_by_non_unit_const p hm = 0 := sorry
+
+lemma irred_div_pq_imp_irred_div_p_or_irred_div_q (p q : polynomial α) (irreducible n : polynomial α) (hc : non_unit_const n): 
+    const_divisor p (and.left hc) ∨ const_divisor q (and.left hc) := sorry
+
+lemma non_unit_const_divisor_imp_non_primitive (p : polynomial α) {q : polynomial α} (hq : non_unit_const q) : const_divisor p (and.left hq) → ¬primitive p := sorry
+
 lemma prod_of_prim_is_prim (p q : polynomial α) : (primitive p ∧ primitive q) → primitive (p * q) := 
 begin 
     intros h_p_q, 
     by_contradiction h_pq, 
     have hp : primitive p := and.left h_p_q,
     have hq : primitive q := and.right h_p_q,
-    have h_div : ∃(m : polynomial α) (hm : non_unit_const m), mod_by_non_unit_const (p * q) hm = 0, by sorry,
-    have h_irred_div : ∃(n : polynomial α) (hn : non_unit_const n), (irreducible n), by sorry,
-
+    have h_div : ∃(m : polynomial α) (hm : non_unit_const m), mod_by_non_unit_const (p * q) hm = 0, exact (h_div_lemma h_pq), 
+    have h_irred_div : ∃(n : polynomial α) (hn : non_unit_const n) (irreducible n), mod_by_non_unit_const (p * q) hn = 0, by sorry, 
+    have h_npq : ¬primitive p ∨ ¬primitive q, by sorry,
+    -- Or elim stuff here
 end
