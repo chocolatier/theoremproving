ab1fc67ce73b86c45aa2c109f507c43e4178d12e
chocolatier <chocolatier@users.noreply.github.com>
2019-01-21 23:39:57 +1100

    rm unnecessary begin-end block

--- src/gauss_lemma.lean
+++ src/gauss_lemma.lean
@@ -51,14 +51,12 @@ begin
 end 
 
 lemma deg_c_times_x_to_n_eq_n (n : ℕ) (c : α) (hc : c ≠ 0) : degree (C c * X^n) = n := 
-begin 
-    have h1: leading_coeff (C c) * leading_coeff X^n ≠ 0, by simp [hc], 
-    show degree (C c * X^n) = n, from calc
-         degree (C c * X^n) = degree (C c) + degree (X^n) : by rw [degree_mul_eq]
-                        ... = 0 + degree (X^n) : by rw [degree_C hc]
-                        ... = 0 + n : by rw degree_X_pow 
-                        ... = n : by simp
-end
+have h1: leading_coeff (C c) * leading_coeff X^n ≠ 0, by simp [hc], 
+show degree (C c * X^n) = n, from calc
+        degree (C c * X^n) = degree (C c) + degree (X^n) : by rw [degree_mul_eq]
+                    ... = 0 + degree (X^n) : by rw [degree_C hc]
+                    ... = 0 + n : by rw degree_X_pow 
+                    ... = n : by simp
 
 
 lemma const_mod_decreasing (hp: ¬is_const p) :
