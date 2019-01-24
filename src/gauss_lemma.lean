d557781c8af9202ce205bf0ff3e27effe16ea8be
Scott Morrison <scott.morrison@anu.edu.au>
2019-01-24 14:28:17 +1100

    minor

--- src/gauss_lemma.lean
+++ src/gauss_lemma.lean
@@ -6,6 +6,8 @@ open polynomial
 open classical
 open with_top
 
+local attribute [instance, priority 0] classical.prop_decidable
+
 universe u 
 
 variables {α : Type u} {a: α} 
@@ -21,6 +23,8 @@ def is_const (p : polynomial α) : Prop := nat_degree p = 0
 
 instance is_const.decidable : decidable (is_const p) :=
 by unfold is_const; apply_instance
+
+#print is_const.decidable
 
 def leading_coeff_non_unit (p : polynomial α) : Prop := ¬is_unit (leading_coeff p) 
 
@@ -46,6 +50,10 @@ begin
     show false, contradiction
 end 
 
+-- TODO this is easier:
+-- lemma not_const_imp_non_zero'  : ¬is_const p → p ≠ (0 : polynomial α) := 
+-- mt begin sorry end
+
 lemma deg_c_times_x_to_n_eq_n (n : ℕ) {c : α} (hc : c ≠ 0) : degree (C c * X^n) = n := 
 have h1: leading_coeff (C c) * leading_coeff X^n ≠ 0, by simp [hc], 
 show degree (C c * X^n) = n, from calc
@@ -64,8 +72,8 @@ end
 
 lemma nat_deg_non_zero_lt_nat_deg_p  {p q : polynomial α} (ha : degree q < degree p) (hp : p ≠ 0) (hq : q ≠ 0) : nat_degree q < nat_degree p := 
 begin 
-    have h1 : ↑(nat_degree q) < ↑(nat_degree p), by {rw [degree_eq_nat_degree hp, degree_eq_nat_degree hq] at ha; exact ha},
-    show nat_degree q < nat_degree p, by{rw with_bot.coe_lt_coe at h1; exact h1}
+    have h1 : ↑(nat_degree q) < ↑(nat_degree p), by {rw [degree_eq_nat_degree hp, degree_eq_nat_degree hq] at ha, exact ha},
+    show nat_degree q < nat_degree p, by{rw with_bot.coe_lt_coe at h1, exact h1}
 end 
 
 lemma const_mod_decreasing (hp: ¬is_const p) :
@@ -109,7 +117,7 @@ def const_divisor : Π (p : polynomial α) {q : polynomial α}, is_const q → P
 -- Maybe better off using GCD coefft = 1? Have UFD α so can produce GCD Domain α...
 def primitive (p : polynomial α) : Prop := ∀(q : polynomial α) (hq: non_unit_const q), (mod_by_non_unit_const p hq ≠ 0)
 
-instance primitive.decidable : decidable (primitive p) := sorry
+-- instance primitive.decidable : decidable (primitive p) := sorry
 
 lemma h_div_lemma {p : polynomial α} (hp : ¬primitive p) : ∃(m : polynomial α) (hm : non_unit_const m), mod_by_non_unit_const p hm = 0 := sorry
 
