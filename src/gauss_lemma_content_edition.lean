691e2889afe84e31dc63facc93d791d9da0e916c
chocolatier <chocolatier@users.noreply.github.com>
2019-01-27 16:15:06 +1100

    type error

--- src/gauss_lemma_content_edition.lean
+++ src/gauss_lemma_content_edition.lean
@@ -17,9 +17,16 @@ variable [decidable_eq α]
 variable [comm_ring α]
 variable [gcd_domain α]
 
+/-gauss_lemma_content_edition.lean:20:0: error
+synthesized type class instance is not definitionally equal to expression inferred by typing rules, synthesized
+  no_zero_divisors.to_has_zero α
+inferred
+  mul_zero_class.to_has_zero α
+-/
+
 def content (p : polynomial α) : ideal α := 
 let 
-    coeffts := map p.to_fun p.support
+    coeffts := p.support.map p.to_fun 
 in 
     span coeffts
   
