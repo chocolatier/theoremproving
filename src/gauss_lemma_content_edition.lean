31d0c1870ec2a5db53383becab128a20ded5eaf1
Scott Morrison <scott@tqft.net>
2019-01-29 09:24:46 -0800

    suggested fix for content
    * I'm not sure what all your variables were for, so I removed some.
    * I think the underlying problem was that `map` on a finset requires an embedding, not a function. You need `image` instead.
    * To diagnose the error (the error message you were seeing was not helpful!) I split out a separate `let` statement to see exactly where it went wrong.

--- src/gauss_lemma_content_edition.lean
+++ src/gauss_lemma_content_edition.lean
@@ -8,14 +8,14 @@ open ideal
 
 local attribute [instance, priority 0] classical.prop_decidable
 
-universe u 
+universe u
 
-variables {α : Type u} {a: α} 
+variables {α : Type u} {a: α}
 
 variables [integral_domain α] {p q r s : polynomial α}
-variable [decidable_eq α]
-variable [comm_ring α]
-variable [gcd_domain α]
+-- variable [decidable_eq α]
+-- variable [comm_ring α]
+-- variable [gcd_domain α]
 
 /-gauss_lemma_content_edition.lean:20:0: error
 synthesized type class instance is not definitionally equal to expression inferred by typing rules, synthesized
@@ -24,9 +24,9 @@ inferred
   mul_zero_class.to_has_zero α
 -/
 
-def content (p : polynomial α) : ideal α := 
-let 
-    coeffts := p.support.map p.to_fun 
-in 
+def content (p : polynomial α) : ideal α :=
+let
+    supp := p.support,
+    coeffts := (supp.image p.to_fun).to_set
+in
     span coeffts
-  
