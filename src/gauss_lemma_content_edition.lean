de04ce898affd44f2583ecae5a61a3f4b2de2055
chocolatier <chocolatier@users.noreply.github.com>
2019-01-30 15:42:39 +1100

    better name for scalar multiplication of content

--- src/gauss_lemma_content_edition.lean
+++ src/gauss_lemma_content_edition.lean
@@ -14,7 +14,6 @@ universe u
 variables {α : Type u} {a: α}
 
 variable [decidable_eq α]
--- variable [comm_ring α]
 variable [gcd_domain α]
 variables [integral_domain α] {p q r s : polynomial α}
 variable [has_one α] -- Exclude the trivial ring
@@ -29,8 +28,11 @@ in
 def is_primitive (p : polynomial α) : Prop :=
 content p = span (singleton (1 : α))
 
+lemma cont_mul_hom_sub {p q : polynomial α} (x : α) (hx : x ∈ content (p * q)) : (x ∈ content p * content q) := sorry
 
-lemma cont_mul_hom (p : polynomial α) (a : α) : (content (C a * p)) = ((span (singleton a)) * (content p)) := sorry
+lemma cont_scalar_mul_fwd (x : α) {p : polynomial α} {a : α} (hx : x ∈ content (C a * p)) : (x ∈ (span (singleton a)) * (content p)) := sorry
+
+lemma cont_scalar_mul (p : polynomial α) (a : α) : (content (C a * p)) = ((span (singleton a)) * (content p)) := sorry
 
 #check gcd (span (singleton α))
 
