fda2bb2674f81e58664257aad7a60ca309213697
chocolatier <chocolatier@users.noreply.github.com>
2019-01-30 16:47:26 +1100

    not define ideal gcd

--- src/gauss_lemma_content_edition.lean
+++ src/gauss_lemma_content_edition.lean
@@ -28,13 +28,14 @@ in
 def is_primitive (p : polynomial α) : Prop :=
 content p = span (singleton (1 : α))
 
-lemma cont_mul_hom_sub {p q : polynomial α} (x : α) (hx : x ∈ content (p * q)) : (x ∈ content p * content q) := sorry
+noncomputable def ideal_gcd (i : ideal α) : (ideal α) := some (λx:ideal α, i ≤ x ∧ (∀(j : ideal α), i ≤ j → x ≤ j))
+
+lemma cont_prod_sub_prod_cont {p q : polynomial α} (x : α) (hx : x ∈ content (p * q)) : (x ∈ content p * content q) := sorry
 
 lemma cont_scalar_mul_fwd (x : α) {p : polynomial α} {a : α} (hx : x ∈ content (C a * p)) : (x ∈ (span (singleton a)) * (content p)) := sorry
 
 lemma cont_scalar_mul (p : polynomial α) (a : α) : (content (C a * p)) = ((span (singleton a)) * (content p)) := sorry
 
-#check gcd (span (singleton α))
+lemma cont_gcd_eq (p : polynomial α) (q : polynomial α) : ideal_gcd (content (p * q)) = ideal_gcd (content p) * ideal_gcd (content q) := sorry
 
 lemma prod_prim_is_prim {p q : polynomial α} (hp: is_primitive p) (hq : is_primitive q)  : is_primitive (p * q) := sorry
-
