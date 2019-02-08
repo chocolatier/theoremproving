ed99341ec51ff7d506e70bbb6de5c23144117186
chocolatier <chocolatier@users.noreply.github.com>
2019-02-08 16:12:12 +1100

    move source example to bottom

--- src/coe_mwe.lean
+++ src/coe_mwe.lean
@@ -13,14 +13,6 @@ variables {α : Type u} {a: α}
 variables [integral_domain α] {p q r s : polynomial α}
 variable [decidable_eq α]
 
-lemma deg_c_times_x_to_n_eq_n (n : ℕ) {c : α} (hc : c ≠ 0) : degree (C c * X^n) = n := 
-have h1: leading_coeff (C c) * leading_coeff X^n ≠ 0, by simp [hc], 
-show degree (C c * X^n) = n, from calc
-        degree (C c * X^n) = degree (C c) + degree (X^n) : by rw [degree_mul_eq]
-                    ... = 0 + degree (X^n) : by rw [degree_C hc]
-                    ... = 0 + n : by rw [degree_X_pow] -- simp [degree_X_pow] also works fine. 
-                    ... = n : by simp
-
 lemma minimal (n : ℕ) : degree (X^n : polynomial α) = n := by squeeze_simp 
 
 lemma step_by_step (n : ℕ) : degree (X^n : polynomial α) = n := 
@@ -32,4 +24,13 @@ end
 
 lemma minimal' (n : ℕ) : add_monoid.smul n 1 = ↑n := by simp -- works 
 
-lemma minimal'' (n : ℕ) : add_monoid.smul n (degree X) = ↑n := by simp
+lemma minimal'' (n : ℕ) : add_monoid.smul n (degree X) = n := by simp
+
+-- Source of error
+lemma deg_c_times_x_to_n_eq_n (n : ℕ) {c : α} (hc : c ≠ 0) : degree (C c * X^n) = n := 
+have h1: leading_coeff (C c) * leading_coeff X^n ≠ 0, by simp [hc], 
+show degree (C c * X^n) = n, from calc
+        degree (C c * X^n) = degree (C c) + degree (X^n) : by rw [degree_mul_eq]
+                    ... = 0 + degree (X^n) : by rw [degree_C hc]
+                    ... = 0 + n : by rw [degree_X_pow] -- simp [degree_X_pow] also works fine. 
+                    ... = n : by simp
