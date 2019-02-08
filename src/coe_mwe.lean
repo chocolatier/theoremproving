443226247143f2b0a49dbffc8e185f414ef8a1d8
chocolatier <chocolatier@users.noreply.github.com>
2019-02-08 16:25:49 +1100

    also add working proof of minimal example, rm minimal', minimal''

--- src/coe_mwe.lean
+++ src/coe_mwe.lean
@@ -15,16 +15,14 @@ variables [integral_domain α]
 
 lemma minimal (n : ℕ) : degree (X^n : polynomial α) = n := by squeeze_simp 
 
+lemma working (n : ℕ) : degree (X^n : polynomial α) = n := by simp [degree_X_pow] -- this works
+
 lemma step_by_step (n : ℕ) : degree (X^n : polynomial α) = n := 
 begin 
    rw polynomial.degree_pow_eq,
    rw polynomial.degree_X,
    rw add_monoid.smul_one
 end 
-
-lemma minimal' (n : ℕ) : add_monoid.smul n 1 = ↑n := by simp -- works 
-
-lemma minimal'' (n : ℕ) : add_monoid.smul n (degree X) = n := by simp
 
 -- Source of error
 lemma deg_c_times_x_to_n_eq_n (n : ℕ) {c : α} (hc : c ≠ 0) : degree (C c * X^n) = n := 
