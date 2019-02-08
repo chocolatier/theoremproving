60427b27d103a4dc39ec058ee644b44a869d2844
chocolatier <chocolatier@users.noreply.github.com>
2019-02-08 15:46:55 +1100

    expand mwe

--- src/coe_mwe.lean
+++ src/coe_mwe.lean
@@ -1,8 +1,13 @@
 import data.polynomial
+import tactic.squeeze
 
 open polynomial
 
+set_option pp.all true
+
 universe u 
+
+local attribute [instance, priority 0] classical.prop_decidable
 
 variables {α : Type u} {a: α} 
 variables [integral_domain α] {p q r s : polynomial α}
@@ -16,4 +21,15 @@ show degree (C c * X^n) = n, from calc
                     ... = 0 + n : by rw [degree_X_pow] -- simp [degree_X_pow] also works fine. 
                     ... = n : by simp
 
-lemma minimal (n : ℕ) : degree (X^n : polynomial α) = n := by simp
+lemma minimal (n : ℕ) : degree (X^n : polynomial α) = n := by squeeze_simp 
+
+lemma step_by_step (n : ℕ) : degree (X^n : polynomial α) = n := 
+begin 
+   rw polynomial.degree_pow_eq,
+   rw polynomial.degree_X,
+   rw add_monoid.smul_one
+end 
+
+lemma minimal' (n : ℕ) : add_monoid.smul n 1 = ↑n := by simp -- works 
+
+lemma minimal'' (n : ℕ) : add_monoid.smul n (degree X) = ↑n := by simp
