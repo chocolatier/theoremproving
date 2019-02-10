5596908e1201962231f825491a646557908fe7ac
chocolatier <chocolatier@users.noreply.github.com>
2019-02-10 19:53:23 +1100

    hitting max class-instance resolution when coercing to quot

--- src/gauss_lemma_content_edition.lean
+++ src/gauss_lemma_content_edition.lean
@@ -20,6 +20,8 @@ variable [gcd_domain α]
 variables [integral_domain α] {p q r s : polynomial α}
 variable [has_one α] -- Exclude the trivial ring
 
+def is_const (p : polynomial α) : Prop := nat_degree p = 0 
+
 def content (p : polynomial α) : ideal α :=
 let
     supp := p.support,
@@ -44,10 +46,15 @@ lemma cont_scalar_mul (p : polynomial α) (a : α) : (content (C a * p)) = ((spa
 
 lemma prod_prim_is_prim {p q : polynomial α} (hp: is_primitive p) (hq : is_primitive q)  : is_primitive (p * q) := sorry
 
-def quot_poly (p : polynomial α) : polynomial (quotient_ring α) := sorry
+def to_quot (a : α) : quotient_ring α := sorry -- ↑a
+
+def quot_poly (p : polynomial α) : polynomial (quotient_ring α) := to_quot ∘ p 
 
 lemma irred_imp_gcd_coeff_1 (p : polynomial α) (hp : irreducible p) : polynomial_coeff_gcd p = 1 := sorry
 
-lemma irred_in_base_imp_irred_in_quot {p : polynomial α} (hp_p : is_primitive p) (hp_ir : irreducible p) (hp_nc : ¬is_const p) : irreducible (quot_poly p) := sorry
+lemma irred_in_base_imp_irred_in_quot {p : polynomial α} (hp_p : is_primitive p) (hp_ir : irreducible p) (hp_nc : ¬is_const p) : irreducible (quot_poly p) := 
+begin 
+  sorry
+end
 
 -- lemma irred_in_quot_imp_irred_in_base {p : polynomial (quotient_ring α)} (hp_ir : irreducible p) 
