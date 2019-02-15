36d555a69df0801ea6247bc0b5445152d2f9ca0b
chocolatier <chocolatier@users.noreply.github.com>
2019-02-15 20:14:42 +1100

    not_irred_imp_non_unit_divisors polymorphic

--- src/gauss_lemma.lean
+++ src/gauss_lemma.lean
@@ -134,7 +134,7 @@ begin
     -- simp [h_divisor, h_non_unit, hp] -- TODO: There should be some lemma that states non unit q and q ∣ p → reducible q. Find it. 
 end
 
-lemma not_irred_imp_non_unit_divisors {p : polynomial α} (hp : ¬irreducible p) (hp' : ¬is_unit p) : ∃(m n : polynomial α),   p = m * n  ∧ (¬ is_unit m) ∧ (¬ is_unit n) :=
+lemma not_irred_imp_non_unit_divisors  {γ : Type*} [monoid γ] [decidable_eq γ] (p : γ) (hp : ¬irreducible p) (hp' : ¬is_unit p) : ∃(m n : γ),   p = m * n  ∧ (¬ is_unit m) ∧ (¬ is_unit n) :=
 begin 
     unfold irreducible at hp,
     rw [not_and_distrib, not_not, not_forall] at hp,
